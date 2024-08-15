6caa23bfe23b80c00c596777773b43c49eb8ea68
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2024-08-15 23:35:35 +0200

    Added `StbImageSharp`, Added `Image` and `AnimatedImage`

--- src/Bliss/CSharp/Textures/Texture.cs
+++ src/Bliss/CSharp/Textures/Texture.cs
@@ -1,9 +1,13 @@
 namespace Bliss.CSharp.Textures;
 
-// TODO DONE IT!
+// TODO: DONE IT!
 public class Texture {
 
     public Texture() {
+        
+    }
+
+    public static Texture Load(string path) {
         
     }
 }
