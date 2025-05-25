1999c235d605bce7192ebd725c63fe303575fe4f
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2025-05-25 19:56:29 +0200

    Reworked Font

--- src/Bliss/CSharp/Fonts/Font.cs
+++ src/Bliss/CSharp/Fonts/Font.cs
@@ -55,6 +55,15 @@ public class Font : Disposable {
         Logger.Info($"Font data loaded successfully from path: [{path}]");
         return File.ReadAllBytes(path);
     }
+    
+    /// <summary>
+    /// Retrieves a <see cref="DynamicSpriteFont"/> object for the specified font size.
+    /// </summary>
+    /// <param name="fontSize">The desired font size for the retrieved sprite font.</param>
+    /// <returns>A <see cref="DynamicSpriteFont"/> instance corresponding to the specified font size.</returns>
+    public DynamicSpriteFont GetSpriteFont(float fontSize) {
+        return this.FontSystem.GetFont(fontSize);
+    }
 
     /// <summary>
     /// Draws text using the specified parameters.
@@ -78,57 +87,55 @@ public class Font : Disposable {
         Vector2 finalOrigin = origin ?? new Vector2(0.0F, 0.0F);
         
         // Draw font.
-        this.FontSystem.GetFont(size).DrawText(batch.FontStashRenderer, text, position, new FSColor(finalColor.R, finalColor.G, finalColor.B, finalColor.A), float.DegreesToRadians(rotation), finalOrigin, scale, depth, characterSpacing, lineSpacing, style, effect, effectAmount);
+        this.GetSpriteFont(size).DrawText(batch.FontStashRenderer, text, position, new FSColor(finalColor.R, finalColor.G, finalColor.B, finalColor.A), float.DegreesToRadians(rotation), finalOrigin, scale, depth, characterSpacing, lineSpacing, style, effect, effectAmount);
     }
-    
+
     /// <summary>
-    /// Measures the dimensions of the specified text using the given font size.
+    /// Measures the dimensions of the specified text using the given font size and other parameters.
     /// </summary>
     /// <param name="text">The text to measure.</param>
     /// <param name="size">The font size to use for measurement.</param>
+    /// <param name="scale">The scale to apply to the text, if any.</param>
+    /// <param name="characterSpacing">Spacing between characters, in pixels.</param>
+    /// <param name="lineSpacing">Spacing between lines, in pixels.</param>
+    /// <param name="effect">Special visual effect to apply when measuring text.</param>
+    /// <param name="effectAmount">The intensity of the specified visual effect.</param>
     /// <returns>A Vector2 representing the width and height of the text.</returns>
-    public Vector2 MeasureText(string text, float size) {
-        DynamicSpriteFont font = this.FontSystem.GetFont(size);
-        
-        Bounds bounds = font.TextBounds(text, Vector2.Zero);
+    public Vector2 MeasureText(string text, float size, Vector2? scale = null, float characterSpacing = 0.0F, float lineSpacing = 0.0F, FontSystemEffect effect = FontSystemEffect.None, int effectAmount = 0) {
+        Bounds bounds = this.GetSpriteFont(size).TextBounds(text, Vector2.Zero, scale, characterSpacing, lineSpacing, effect, effectAmount);
         return new Vector2(bounds.X2, bounds.Y2);
     }
 
     /// <summary>
-    /// Measures the dimensions of the specified trimmed text using the given font size.
+    /// Measures the dimensions of the specified trimmed text using the provided font size.
     /// </summary>
     /// <param name="text">The text to measure.</param>
     /// <param name="size">The size of the font.</param>
+    /// <param name="scale">An optional scaling factor for the text. Defaults to null.</param>
+    /// <param name="characterSpacing">The spacing between characters. Defaults to 0.0.</param>
+    /// <param name="lineSpacing">The spacing between lines. Defaults to 0.0.</param>
+    /// <param name="effect">The text rendering effect to apply. Defaults to <see cref="FontSystemEffect.None"/>.</param>
+    /// <param name="effectAmount">The magnitude of the effect to apply. Defaults to 0.</param>
     /// <returns>A <see cref="Vector2"/> representing the width and height of the trimmed text.</returns>
-    public Vector2 MeasureTextTrimmed(string text, float size) {
-        DynamicSpriteFont font = this.FontSystem.GetFont(size);
-        
-        Bounds bounds = font.TextBounds(text, Vector2.Zero);
+    public Vector2 MeasureTextTrimmed(string text, float size, Vector2? scale = null, float characterSpacing = 0.0F, float lineSpacing = 0.0F, FontSystemEffect effect = FontSystemEffect.None, int effectAmount = 0) {
+        Bounds bounds = this.GetSpriteFont(size).TextBounds(text, Vector2.Zero, scale, characterSpacing, lineSpacing, effect, effectAmount);
         return new Vector2(bounds.X2 - bounds.X, bounds.Y2 - bounds.Y);
     }
 
     /// <summary>
-    /// Measures the rectangular bounds of the given text with specified font size.
+    /// Measures the rectangular bounds of the specified text with the given font size and additional text settings.
     /// </summary>
-    /// <param name="text">The text to measure.</param>
-    /// <param name="size">The size of the font.</param>
-    /// <return>The bounds of the text as a Rectangle.</return>
-    public RectangleF MeasureTextRect(string text, float size) {
-        DynamicSpriteFont font = this.FontSystem.GetFont(size);
-        
-        Bounds bounds = font.TextBounds(text, Vector2.Zero);
+    /// <param name="text">The text to be measured.</param>
+    /// <param name="size">The size of the font used for measurement.</param>
+    /// <param name="scale">Optional scale to be applied to the text. Defaults to null for no scaling.</param>
+    /// <param name="characterSpacing">Optional additional spacing between characters. Defaults to 0.0.</param>
+    /// <param name="lineSpacing">Optional additional spacing between lines. Defaults to 0.0.</param>
+    /// <param name="effect">Optional font effect to apply. Defaults to <see cref="FontSystemEffect.None"/>.</param>
+    /// <param name="effectAmount">Optional intensity of the font effect. Defaults to 0.</param>
+    /// <returns>A <see cref="RectangleF"/> representing the bounds of the measured text.</returns>
+    public RectangleF MeasureTextRect(string text, float size, Vector2? scale = null, float characterSpacing = 0.0F, float lineSpacing = 0.0F, FontSystemEffect effect = FontSystemEffect.None, int effectAmount = 0) {
+        Bounds bounds = this.GetSpriteFont(size).TextBounds(text, Vector2.Zero, scale, characterSpacing, lineSpacing, effect, effectAmount);
         return new RectangleF(bounds.X, bounds.Y, bounds.X2, bounds.Y2);
-    }
-
-    /// <summary>
-    /// Creates and initializes a new instance of the <see cref="FontStashSharp.FontSystem"/> class using the current font data.
-    /// </summary>
-    /// <returns>A newly created <see cref="FontStashSharp.FontSystem"/> instance.</returns>
-    private FontSystem CreateFontSystem() {
-        FontSystem system = new FontSystem(new FontSystemSettings());
-        system.AddFont(this.FontData);
-
-        return system;
     }
     
     protected override void Dispose(bool disposing) {
