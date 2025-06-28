b0bfeb994c4aa491ed4b495ba3c561bd67a7dadb
Legend286 <asagpol@gmail.com>
2025-06-28 01:29:05 +0100

    Unbreak code style

--- src/Bliss/CSharp/Mathematics/BlissMath.cs
+++ src/Bliss/CSharp/Mathematics/BlissMath.cs
@@ -35,8 +35,7 @@ public static class BlissMath {
     /// <param name="yaw">The yaw angle in degrees.</param>
     /// <param name="roll"> The roll angle in degrees.</param>
     /// <returns> A <see cref="Quaternion"/> representing the absolute rotation given the angles provided.</returns>
-    public static Quaternion RotationEulerDegrees(float pitch, float yaw, float roll)
-    {
+    public static Quaternion RotationEulerDegrees(float pitch, float yaw, float roll) {
         return Quaternion.CreateFromYawPitchRoll(float.DegreesToRadians(yaw), float.DegreesToRadians(pitch), float.DegreesToRadians(roll));
     }
     
@@ -46,8 +45,7 @@ public static class BlissMath {
     /// <param name="yaw">The yaw angle in degrees.</param>
     /// <param name="roll"> The roll angle in degrees.</param>
     /// <returns> A <see cref="Quaternion"/> representing the rotation delta between frames given the angles provided.</returns>
-    public static Quaternion RotationDeltaEulerDegrees(float pitch, float yaw, float roll, float deltaTime)
-    {
+    public static Quaternion RotationDeltaEulerDegrees(float pitch, float yaw, float roll, float deltaTime) {
         return Quaternion.CreateFromYawPitchRoll(float.DegreesToRadians(yaw * deltaTime), float.DegreesToRadians(pitch * deltaTime), float.DegreesToRadians(roll * deltaTime));
     }
 }
