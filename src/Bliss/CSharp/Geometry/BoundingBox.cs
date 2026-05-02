c446243a0836d784fd0982f1d0d2c224ae4671e4
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2026-05-02 22:26:07 +0200

    Added IEqual to BoundingBox

--- src/Bliss/CSharp/Geometry/BoundingBox.cs
+++ src/Bliss/CSharp/Geometry/BoundingBox.cs
@@ -2,7 +2,7 @@ using System.Numerics;
 
 namespace Bliss.CSharp.Geometry;
 
-public struct BoundingBox {
+public struct BoundingBox : IEquatable<BoundingBox> {
 
     /// <summary>
     /// Minimum box-corner.
@@ -22,5 +22,55 @@ public struct BoundingBox {
     public BoundingBox(Vector3 min, Vector3 max) {
         this.Min = min;
         this.Max = max;
+    }
+    
+    /// <summary>
+    /// Determines whether two specified bounding boxes are equal.
+    /// </summary>
+    /// <param name="left">The first bounding box to compare.</param>
+    /// <param name="right">The second bounding box to compare.</param>
+    /// <returns><c>true</c> if the bounding boxes are equal; otherwise, <c>false</c>.</returns>
+    public static bool operator ==(BoundingBox left, BoundingBox right) => left.Equals(right);
+    
+    /// <summary>
+    /// Determines whether two specified bounding boxes are not equal.
+    /// </summary>
+    /// <param name="left">The first bounding box to compare.</param>
+    /// <param name="right">The second bounding box to compare.</param>
+    /// <returns><c>true</c> if the bounding boxes are not equal; otherwise, <c>false</c>.</returns>
+    public static bool operator !=(BoundingBox left, BoundingBox right) => !left.Equals(right);
+    
+    /// <summary>
+    /// Determines whether the current bounding box is equal to another bounding box.
+    /// </summary>
+    /// <param name="other">The bounding box to compare with the current bounding box.</param>
+    /// <returns><c>true</c> if the current bounding box is equal to <paramref name="other"/>; otherwise, <c>false</c>.</returns>
+    public bool Equals(BoundingBox other) {
+        return this.Min.Equals(other.Min) && this.Max.Equals(other.Max);
+    }
+    
+    /// <summary>
+    /// Determines whether the specified object is equal to the current bounding box.
+    /// </summary>
+    /// <param name="obj">The object to compare with the current bounding box.</param>
+    /// <returns><c>true</c> if the specified object is a <see cref="BoundingBox"/> equal to the current bounding box; otherwise, <c>false</c>.</returns>
+    public override bool Equals(object? obj) {
+        return obj is BoundingBox other && this.Equals(other);
+    }
+    
+    /// <summary>
+    /// Returns the hash code for this bounding box.
+    /// </summary>
+    /// <returns>A hash code for the current bounding box.</returns>
+    public override int GetHashCode() {
+        return HashCode.Combine(this.Min.GetHashCode(), this.Max.GetHashCode());
+    }
+    
+    /// <summary>
+    /// Returns a string that represents the current bounding box.
+    /// </summary>
+    /// <returns>A string containing the minimum and maximum corners of the bounding box.</returns>
+    public override string ToString() {
+        return $"Min:{this.Min} Max:{this.Max}";
     }
 }
